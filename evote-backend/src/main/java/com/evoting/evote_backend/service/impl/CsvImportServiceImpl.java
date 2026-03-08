973415d0e47c4b341fd9ce594a75c7514e441606
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2026-03-08 17:15:28 +0100

    Backend: fix: added CsvImportService for refactoring and singl responsabilty principle

---
+++ evote-backend/src/main/java/com/evoting/evote_backend/service/impl/CsvImportServiceImpl.java
@@ -0,0 +1,64 @@
+package com.evoting.evote_backend.service.impl;
+
+import com.evoting.evote_backend.dto.VoterDTO;
+import com.evoting.evote_backend.service.interfaces.CsvImportService;
+import com.opencsv.CSVReader;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class CsvImportServiceImpl implements CsvImportService {
+
+    @Override
+    public List<VoterDTO> parseVotersCsv(MultipartFile file) {
+
+        List<VoterDTO> voters = new ArrayList<>();
+
+        try (Reader reader = new InputStreamReader(file.getInputStream())) {
+
+            CSVReader csvReader = new CSVReader(reader);
+
+            List<String[]> rows = csvReader.readAll();
+
+            if (rows.size() <= 1) {
+                throw new IllegalArgumentException("CSV must contain voters");
+            }
+
+            Set<String> emails = new HashSet<>();
+
+            for (int i = 1; i < rows.size(); i++) {
+
+                String[] row = rows.get(i);
+
+                String name = row[0].trim();
+                String email = row[1].trim();
+
+                if (!isValidEmail(email)) {
+                    throw new IllegalArgumentException("Invalid email: " + email);
+                }
+
+                if (!emails.add(email)) {
+                    throw new IllegalArgumentException("Duplicate email: " + email);
+                }
+
+                voters.add(new VoterDTO(name, email));
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to parse CSV", e);
+        }
+
+        return voters;
+    }
+
+    private boolean isValidEmail(String email) {
+        return email.matches("^[A-Za-z0-9+_.-]+@(.+)$");
+    }
+}
---
+++ evote-backend/src/main/java/com/evoting/evote_backend/service/interfaces/CsvImportService.java
@@ -0,0 +1,11 @@
+package com.evoting.evote_backend.service.interfaces;
+
+import com.evoting.evote_backend.dto.VoterDTO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface CsvImportService {
+    List<VoterDTO> parseVotersCsv(MultipartFile file);
+
+}
