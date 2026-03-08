229a8377c68c32f463e9c4e2ebb5225ef4b6d57a
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2026-03-08 17:17:53 +0100

    Backend: feat: added BusinessException class

--- evote-backend/src/main/java/com/evoting/evote_backend/exception/GlobalExceptionHandler.java
+++ evote-backend/src/main/java/com/evoting/evote_backend/exception/GlobalExceptionHandler.java
@@ -1,20 +1,21 @@
 package com.evoting.evote_backend.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().body("Erreur de requête : " + e.getMessage());
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<String> handleBusinessExceptions(BusinessException e) {
+        return ResponseEntity.badRequest().body("Request Error : " + e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleAll(Exception e) {
-        e.printStackTrace(); // utile temporairement
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erreur interne : " + e.getMessage());
+    public ResponseEntity<String> handleGeneric(Exception e) {
+        return ResponseEntity.status(500)
+                .body("Une erreur interne est survenue");
     }
+
+
 }
