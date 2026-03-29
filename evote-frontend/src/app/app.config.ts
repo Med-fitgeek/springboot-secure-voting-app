15a0c2f196ab68c732c8f876c21617af182119dd
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2026-03-29 16:00:04 +0200

    frontend: fix: updated angular configarations

--- evote-frontend/src/app/app.config.ts
+++ evote-frontend/src/app/app.config.ts
@@ -1,16 +1,23 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withInterceptors([jwtInterceptor])),
     provideRouter(routes),
+
+    provideHttpClient(
+      withInterceptors([jwtInterceptor])
+    ),
+
     provideAnimations(),
+
     provideToastr(),
   ]
 };
--- evote-frontend/src/main.ts
+++ evote-frontend/src/main.ts
@@ -1,21 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideToastr } from 'ngx-toastr';
-import { jwtInterceptor } from './app/core/interceptors/jwt.interceptor';
+import { appConfig } from './app/app.config';
 
-
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    importProvidersFrom(BrowserAnimationsModule),
-    provideToastr(),
-    provideHttpClient(withInterceptors([jwtInterceptor])),
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
